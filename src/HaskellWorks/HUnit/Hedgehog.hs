d1d0714ffb49da2d7e36bf4f378acd9aaa04ae84
John Ky <newhoggy@gmail.com>
2017-04-24 20:04:20 +1000

    Initial implementation

--- src/HaskellWorks/HUnit/Hedgehog.hs
+++ src/HaskellWorks/HUnit/Hedgehog.hs
@@ -1,6 +1,23 @@
-module HaskellWorks.HUnit.Hedgehog
-  ( someFunc
-  ) where
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module HaskellWorks.HUnit.Hedgehog where
+
+import           Hedgehog
+
+import qualified Control.Exception      as E
+import           Control.Monad          (unless)
+import           Control.Monad.IO.Class
+import           Data.CallStack
+import           Test.HUnit.Lang
+
+location :: HasCallStack => Maybe SrcLoc
+location = case reverse callStack of
+  (_, loc) : _ -> Just loc
+  []           -> Nothing
+
+require :: HasCallStack => Property -> Assertion
+require p = do
+  result <- liftIO $ check $ p
+  unless result $ do
+    (E.throwIO (HUnitFailure location $ Reason "Hedgehog property test failed"))
--- test/HaskellWorks/HUnit/HedgehogSpec.hs
+++ test/HaskellWorks/HUnit/HedgehogSpec.hs
@@ -2,11 +2,13 @@
 
 module HaskellWorks.HUnit.HedgehogSpec (spec) where
 
+import           HaskellWorks.HUnit.Hedgehog
+import           Hedgehog
 import           Test.Hspec
 
 {-# ANN module ("HLint: ignore Redundant do"  :: String) #-}
 
 spec :: Spec
 spec = describe "HaskellWorks.HUnit.HedgehogSpec" $ do
-  it "stub" $ do
-    True `shouldBe` True
+  it "should have require function that checks hedgehog properties" $ do
+    require $ property $ 42 === 41
